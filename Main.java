004cf38dbd35ee1bfe5875fd4c3e7c29a3cbd804
AlexeyGold2077 <alexgold2077@yandex.ru>
2023-12-23 19:14:32 +0300

    Fix the static issue

--- Main.java
+++ Main.java
@@ -4,18 +4,16 @@ import java.security.NoSuchAlgorithmException;
 
 public class Main {
 
-    public String strToSHA256(String input) throws NoSuchAlgorithmException {
+    public static String strToSHA256(String input) throws NoSuchAlgorithmException {
         MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
         byte[] bHash = messageDigest.digest(input.getBytes());
         BigInteger iHash = new BigInteger(1, bHash);
         return iHash.toString(16);
     }
-    
+
     public static void main(String[] args) throws NoSuchAlgorithmException {
         String strToHash = "Alexey lost 10k :)";
-        
-        POW hasher = new POW();
 
-        System.out.println(hasher.strToSHA256(strToHash));
+        System.out.println(strToSHA256(strToHash));
     }
 }

17f0eb006a4fd83219c10a272d77b0eb594a137f
AlexeyGold2077 <alexgold2077@yandex.ru>
2023-12-23 19:13:38 +0300

    Delete POW class

--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Main {
+
+    public String strToSHA256(String input) throws NoSuchAlgorithmException {
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        byte[] bHash = messageDigest.digest(input.getBytes());
+        BigInteger iHash = new BigInteger(1, bHash);
+        return iHash.toString(16);
+    }
+    
     public static void main(String[] args) throws NoSuchAlgorithmException {
         String strToHash = "Alexey lost 10k :)";
         

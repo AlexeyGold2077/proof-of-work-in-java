04638346a9a9f8d9c03c5f1144adb052997f9661
AlexeyGold2077 <alexgold2077@yandex.ru>
2023-12-24 17:50:16 +0300

    Change function names

--- Main.java
+++ Main.java
@@ -4,14 +4,14 @@ import java.security.NoSuchAlgorithmException;
 
 public class Main {
 
-    public static String strToSHA256(String input) throws NoSuchAlgorithmException {
+    public static String SHA256(String input) throws NoSuchAlgorithmException {
         MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
         byte[] bHash = messageDigest.digest(input.getBytes());
         BigInteger iHash = new BigInteger(1, bHash);
         return iHash.toString(16);
     }
 
-    public static boolean isEnoughZeros(String str, int zeros) {
+    public static boolean checkZeros(String str, int zeros) {
         int iter = 0;
         while(str.charAt(iter) == '0') { iter++; }
         if(iter >= zeros) { return true; }
@@ -20,14 +20,10 @@ public class Main {
 
     public static void main(String[] args) throws NoSuchAlgorithmException {
         String strToHash = "Alexey";
-
         BigInteger iter = new BigInteger("0");
 
-        while(!isEnoughZeros(strToSHA256(strToHash + String.valueOf(iter)), 1)) {
-            iter.add(BigInteger.ONE);
-            //System.out.println(strToHash + String.valueOf(iter) + "-" + String.valueOf(iter) + "-" + strToSHA256(strToHash + String.valueOf(iter)));
-        }
+        while(!checkZeros(SHA256(strToHash + String.valueOf(iter)), 1)) { iter.add(BigInteger.ONE); }
 
-        System.out.println(strToHash + String.valueOf(iter) + "-" + strToSHA256(strToHash + String.valueOf(iter)));
+        System.out.println(strToHash + String.valueOf(iter) + "-" + SHA256(strToHash + String.valueOf(iter)));
     }
 }
